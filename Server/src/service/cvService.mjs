ba2e04f2d393ad569efc6b31b155814872cbd30f
Ruchira Ranasinghe <it20068028@my.sliit.lk>
2023-05-17 10:58:24 +0530

    added vacancy and CV service
    addded vacancyService.mjs and cvService.mjs

---
+++ Server/src/service/cvService.mjs
@@ -0,0 +1,49 @@
+import { CustomError, BadRequestError } from "../error/index.mjs";
+import CVModel from "../models/cv.model.mjs";
+
+export const getCVS = async ({ search = "", sortBy = "createdAt", order = "-1", limit = "2", page = "1" }) => {
+  try {
+    return await CVModel.find()
+      .sort({ [sortBy]: order })
+      .limit(parseInt(limit))
+      .skip((parseInt(page) - 1) * parseInt(limit));
+  } catch (error) {
+    throw new CustomError(error.message);
+  }
+};
+
+export const getCV = async (id) => {
+  try {
+    const cv = await CVModel.findById(id);
+    if (!cv) {
+      throw new BadRequestError("Budget not found");
+    }
+  } catch (error) {
+    throw new CustomError(error.message);
+  }
+};
+
+export const createCV = async (CV) => {
+  const newCV = new CVModel(CV);
+  try {
+    return await CVModel.save();
+  } catch (error) {
+    throw new CustomError(error.message);
+  }
+};
+
+export const deleteCV = async (id) => {
+  try {
+    await CVModel.findByIdAndDelete(id);
+  } catch (error) {
+    throw new CustomError(error.message);
+  }
+};
+
+export const updateCV = async (id, CV) => {
+  try {
+    return await CVModel.findByIdAndUpdate(id, CV, { new: true });
+  } catch (error) {
+    throw new CustomError(error.message);
+  }
+};
